713f14458a38e68639a700dffa596ae631fced5a
Daniel Hong <danielhong35@gmail.com>
2026-08-28 18:04:04 -0700

    Added function to draw subdivision visualization

--- stringArt.js
+++ stringArt.js
@@ -6,11 +6,24 @@ function StringArt(p1, p2, p3, subdivisions) {
 }
 
 StringArt.prototype.draw = function() {
+    this.drawWeb();
+}
+
+StringArt.prototype.drawWeb = function() {
     // Connect points
     strokeWeight(5);
     stroke(45, 247, 244);
     line(this.p1[0], this.p1[1], this.p2[0], this.p2[1]);
     line(this.p2[0], this.p2[1], this.p3[0], this.p3[1]);
+
+    // Draw subdivisions
+    strokeWeight(5);
+    stroke(171, 255, 25);
+    var l1_x = Math.abs(this.p2[0] - this.p1[0]);
+    var l1_y = Math.abs(this.p2[1] - this.p1[1]);
+    for (var i = 1; i <= this.subdivisions; i++) {
+        point(this.p1[0] + i * l1_x / -~this.subdivisions, this.p1[1] + i * l1_y / -~this.subdivisions);
+    }
 
     // Draw points
     strokeWeight(10);
